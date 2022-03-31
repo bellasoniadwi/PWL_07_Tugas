4ce6f04502655e6783828b4e5c6c1bb9c66ac3dd
bellasoniadwi <bellasoniadwi@gmail.com>
2022-03-31 14:33:13 +0700

    add new function 'search'

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -15,9 +15,24 @@ class MahasiswaController extends Controller
     public function index()
     {
         //fungsi eloquent menampilkan data menggunakan pagination
-        $mahasiswa = Mahasiswa::all(); // Mengambil semua isi tabel
-        $posts = Mahasiswa::orderBy('nim', 'desc')->paginate(6);
+        $mahasiswa = Mahasiswa::paginate(5); // Mengambil semua isi tabel
+        $posts = Mahasiswa::orderBy('nim', 'asc')->paginate(5);
         return view('mahasiswa.index', compact('mahasiswa'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Find a listing of the searching.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function search(Request $request)
+    {
+        $keyword = $request->search;
+ 
+        $mahasiswa = Mahasiswa::where('nama', 'like', "%" . $keyword . "%")->paginate(5);
+        return view('mahasiswa.cari', compact('mahasiswa'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
@@ -43,9 +58,11 @@ class MahasiswaController extends Controller
         $request->validate([
             'nim' => 'required',
             'nama' => 'required',
+            'tanggal_lahir' => 'required',
             'kelas' => 'required',
             'jurusan' => 'required',
             'no_handphone' => 'required',
+            'email' => 'required',
         ]);
 
         //fungsi eloquent untuk menambah data
@@ -95,9 +112,11 @@ class MahasiswaController extends Controller
         $request->validate([
             'nim' => 'required',
             'nama' => 'required',
+            'tanggal_lahir' => 'required',
             'kelas' => 'required',
             'jurusan' => 'required',
             'no_handphone' => 'required',
+            'email' => 'required',
         ]);
         //fungsi eloquent untuk mengupdate data inputan kita
         Mahasiswa::find($nim)->update($request->all());
@@ -119,4 +138,19 @@ class MahasiswaController extends Controller
         return redirect()->route('mahasiswa.index')
             ->with('success', 'Mahasiswa Berhasil Dihapus');
     }
+
+    /**
+     * Penambahan function baru 'cari'
+     *
+     */
+    /*
+    public function cari(Request $request)
+	{
+        $keyword = $request->cari;
+        $mahasiswa = Mahasiswa::where('nama', 'like', "%" . $keyword . "%");
+        return view('mahasiswa.cari', compact('mahasiswa'))
+        ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+    */
+    
 }
