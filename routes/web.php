9d5f53e96ba0ef11201ab510496a7d0158381679
bellasoniadwi <bellasoniadwi@gmail.com>
2022-03-31 14:38:25 +0700

    add route to 'search'

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('mahasiswa', MahasiswaController::class);
 
+Route::get('cari',[MahasiswaController::class, 'search']);
+
 Route::get('/', function () {
     return view('welcome');
 });
